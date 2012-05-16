f38b832e3859aab1b87cbd6105d0bfc2df35f440
Feng Liu <liufeng@cnliufeng.com>
2012-05-16 02:56:28 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlBasket::Application.config.secret_token = '09971884dfe14c7e06dd67dcfaec7a72cad32e1f0fee80cdc0b10d13bbf16ad1023d16447aad0460a1047955485531face94489a181d1c660cde32cdb6597750'
